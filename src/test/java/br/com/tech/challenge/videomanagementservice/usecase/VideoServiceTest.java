125199ef4630ff8f2a1460113c34ba1cf8389e5e
Handryo Casali <handryo@hotmail.com>
2025-04-15 19:51:49 -0300

    add sonar

---
+++ src/test/java/br/com/tech/challenge/videomanagementservice/usecase/VideoServiceTest.java
@@ -0,0 +1,16 @@
+package br.com.tech.challenge.videomanagementservice.usecase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class VideoServiceTest {
+
+    @Test
+    void testeTrue(){
+        assertTrue(true);
+    }
+}
